ca36890c49e43f3aa136ed757743cfdd1f8ed7d0
Etxe <ietxebarriao@ayesa.com>
2025-08-14 15:27:29 +0200

    AzureTestSollube

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🚀 Hola desde Bermeo"
+    return "🚀 Hola desde Sollube"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
